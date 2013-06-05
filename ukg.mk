6e3db423b59ce4859aac2337a338640159c425b3
Daniel MM <daxx4182@gmail.com>
2013-06-06 04:46:19 +0800

    Initial Commit
    - Big thanks to CM and @victorator -

---
+++ ukg.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013 The Xylon Experimental
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# name
+PRODUCT_RELEASE_NAME := i9505
+
+# device
+$(call inherit-product, device/samsung/i9505/full_i9505.mk)
+
+# gsm
+$(call inherit-product, vendor/ukg/config/common_gsm.mk)
+
+# languages
+PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
+
+# phone
+$(call inherit-product, vendor/ukg/config/common_phone.mk)
+
+# products
+PRODUCT_DEVICE := i9505
+PRODUCT_NAME := ukg_i9505
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=ukg.i9505.$(shell date +%m%d%y).$(shell date +%H%M%S)
+
+# overrides  
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=573038 \
+    PRODUCT_NAME=jfltexx \
+    TARGET_BUILD_TYPE=user \
+    TARGET_DEVICE=jflte \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys" \
+    BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys"
+
+# vendorfiles
+PRODUCT_COPY_FILES +=  \
+    vendor/ukg/prebuilt/hybrid_m7_no_nav.conf:system/etc/beerbong/properties.conf \
+    vendor/ukg/prebuilt/bootanimation/bootanimation_1280_768.zip:system/media/bootanimation.zip
+
